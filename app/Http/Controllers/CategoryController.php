e4a9eba35d7ca0af0d3d842e47dfade86d88f50e
Abdul Majid <abdulmajidcse@gmail.com>
2024-12-13 00:37:24 +0600

    category crud completed.

---
+++ app/Http/Controllers/CategoryController.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Category;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
+
+class CategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        $data['categories'] = Category::withCount('products')->latest()->paginate(10);
+
+        return view('category.index', $data);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('category.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $validatedData = $request->validate([
+            'name' => ['required', 'string', 'max:190', Rule::unique(Category::class)],
+        ]);
+
+        Category::create($validatedData);
+
+        return redirect()->route('categories.index')->with(['message' => 'Category Created Successfully!', 'type' => 'success']);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Category $category)
+    {
+        return view('category.create', ['category' => $category]);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Category $category)
+    {
+        $validatedData = $request->validate([
+            'name' => ['required', 'string', 'max:190', Rule::unique(Category::class)->ignore($category->id)],
+        ]);
+
+        $category->update($validatedData);
+
+        return redirect()->route('categories.index')->with(['message' => 'Category Updated Successfully!', 'type' => 'success']);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Category $category)
+    {
+        $category->delete();
+
+        return redirect()->route('categories.index')->with(['message' => 'Category Deleted Successfully!', 'type' => 'success']);
+    }
+}
--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -22,6 +22,11 @@
                         {{ __('Frontend') }}
                     </x-nav-link>
                 </div>
+                <div class="hidden space-x-8 sm:-my-px sm:ms-10 sm:flex">
+                    <x-nav-link :href="route('categories.index')" :active="request()->routeIs('categories.index')">
+                        {{ __('Category') }}
+                    </x-nav-link>
+                </div>
             </div>
 
             <!-- Settings Dropdown -->
@@ -86,6 +91,9 @@
             </x-responsive-nav-link>
             <x-responsive-nav-link :href="url('/')">
                 {{ __('Frontend') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('categories.index')" :active="request()->routeIs('categories.index')">
+                {{ __('Category') }}
             </x-responsive-nav-link>
         </div>
 
